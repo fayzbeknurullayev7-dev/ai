4764a2ed59b8874039df4cfc0a88890c86aa43f1
root <root@localhost.localdomain>
2026-06-13 12:28:08 +0500

    Fix: backend IP 192.168.1.5

--- flutter/lib/core/constants/api_constants.dart
+++ flutter/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const baseUrl = 'http://10.0.2.2:8000/api/v1'; // Android emulator
+  static const baseUrl = 'http://192.168.1.5:8000/api/v1'; // Android emulator
   // static const baseUrl = 'http://localhost:8000/api/v1'; // iOS/Web
 
   static const chatEndpoint = '/chat/';
